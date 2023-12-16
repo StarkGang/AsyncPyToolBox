6199303d32e37d2d35fde9adc3a957e3a7bc2792
Midhun K M <StarkGangZ@gmail.com>
2023-12-16 17:41:48 +0530

    yesy

--- AsyncPyToolbox/errors.py
+++ AsyncPyToolbox/errors.py
@@ -1,8 +1,8 @@
 # Copyright (C) 2023-present by TelegramExtended@Github, < https://github.com/TelegramExtended >.
 #
-# This file is part of < https://github.com/TelegramExtended/TelegramExtended > project,
+# This file is part of < https://github.com/TelegramExtended/AsyncPyToolBox > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TelegramExtended/TelegramExtended/blob/main/LICENSE >
+# Please see < https://github.com/TelegramExtended/AsyncPyToolBox/blob/main/LICENSE >
 #
 # All rights reserved.
 
